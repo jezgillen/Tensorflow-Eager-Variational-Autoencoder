dc2cb803399de40e2bdd65976fe73be27b4bb11a
Jeremy Gillen <jez.gillen@gmail.com>
2019-01-11 19:08:52 +1030

    Added docstrings and cleaned up sampling

--- VAE.py
+++ VAE.py
@@ -16,11 +16,18 @@ class BasicVAE():
                  encoder_hidden_units = 200, decoder_hidden_units = 200):
 
         self.input_size = input_size = np.prod(input_shape)
-        w1 = tf.Variable(tf.random_normal([input_size, encoder_hidden_units], 0.0, 0.001))
-        w2 = tf.Variable(tf.random_normal([encoder_hidden_units, NumLatentVars*2], 0.0, 0.001))
 
-        w3 = tf.Variable(tf.random_normal([NumLatentVars, decoder_hidden_units], 0.0, 0.001))
-        w4 = tf.Variable(tf.random_normal([decoder_hidden_units, input_size], 0.0, 0.001))
+        # Initialise encoder weights 
+        w1 = tf.Variable(tf.random_normal(
+            [input_size, encoder_hidden_units], 0.0, 0.001))
+        w2 = tf.Variable(tf.random_normal(
+            [encoder_hidden_units, NumLatentVars*2], 0.0, 0.001))
+
+        # Initialise decoder weights
+        w3 = tf.Variable(tf.random_normal(
+            [NumLatentVars, decoder_hidden_units], 0.0, 0.001))
+        w4 = tf.Variable(tf.random_normal(
+            [decoder_hidden_units, input_size], 0.0, 0.001))
 
         self.weights = [w1,w2,w3,w4]
 
@@ -30,31 +37,39 @@ class BasicVAE():
         return logit
 
     def decoder(self, z):
-        h = tf.nn.relu(z@self.weights[2])
-        logit = h@self.weights[3]
+        """ 
+        This function expects an input shape of [sample, batch, vector]
+        This is accounted for by expanding the shape of the weights
+        to: [1, input_dim, output_dim].
+        """
+        h = tf.nn.relu(z@tf.expand_dims(self.weights[2],0))
+        logit = h@tf.expand_dims(self.weights[3],0)
         return logit
 
-    # This function assumes data in the range [0,1]
-    # If l=1 (default), then returns shape (batch_size, input_size)
-    # If l!=1, then returns shape (l, batch_size, input_size)
     def predict(self, X, l=1, return_logit_and_parameters=False):
+        """
+        This function assumes data in the range [0,1]
+        If l=1 (default), then returns shape (batch_size, input_size)
+        If l!=1, then returns shape (l, batch_size, input_size)
+        """
         logit, _ = self._predict(X, l)
         return tf.nn.sigmoid(logit)
 
-    # Function for internal use, returns logit and parameters
     def _predict(self, X, l=1):
+        """
+        Function for internal use, returns logit and parameters
+        """
         parameters_layer = self.encoder(X)
         z_sample, z_parameters = self.g(parameters_layer,num_samples=l)
-        #reshape so new batch size is l*original_batch size
-        z_sample = tf.reshape(z_sample, (-1, z_sample.shape[-1]))
+        #  z_sample is of shape [num_samples, batch_size, vectors]
         logit = self.decoder(z_sample)
-        if(l!=1):
-            logit = tf.reshape(logit, (l,-1, self.input_size))
 
         return logit, z_parameters
 
-    # Yields control back every epoch, returning the average ELBO across batches
     def train(self, X, num_epochs=10, batch_size=100):
+        """
+        Yields control back every epoch, returning the average ELBO across batches
+        """
         opt = tf.train.AdagradOptimizer(0.01)
         for i in range(num_epochs):
             loss_history = []
@@ -76,24 +91,32 @@ class BasicVAE():
         logit, z_parameters = self._predict(X)
         z_mu, z_sigma = z_parameters
         # Separate KL divergence and Expected reconstruction Loss
-        logp = self.reconstructionLoss(logit, X)
+        logp = self._reconstructionLoss(logit, X)
         kl = self.KLdivergence((z_mu, z_sigma))
         elbo = logp - kl
         return elbo
 
-    def reconstructionLoss(self, logits, labels):
-        # uses my version of sigmoid cross entropy
-        cross_entropy = sigmoid_cross_entropy_with_logits(labels=labels,logits=logits)
+    def _reconstructionLoss(self, logits, labels):
+        # This accounts for an extra sampling dimension during training
+        if(tf.rank(logits).numpy() == 3):
+            labels = tf.expand_dims(labels, 0)
 
-        # Account for multiple samples
-        if(tf.rank(cross_entropy) == 3):
-            cross_entropy = tf.reduce_mean(cross_entropy,axis=0)
+        loss = self.reconstructionLoss(logits, labels)
 
-        # Bernoulli loss
+        if(tf.rank(loss).numpy() == 3):
+            # Average across samples
+            loss = tf.reduce_mean(loss,axis=0)
+
+        # Average across batch, then sum across elements of output vector
         logp = -tf.reduce_sum(
-                tf.reduce_mean(cross_entropy,axis=0),
+                tf.reduce_mean(loss,axis=0),
                 axis=-1)
+
         return logp
+
+    def reconstructionLoss(self, logits, labels):
+        return tf.nn.sigmoid_cross_entropy_with_logits(labels=labels,logits=logits)
+
 
     def KLdivergence(self, z_parameters):
         z_mu, z_sigma = z_parameters
@@ -114,7 +137,7 @@ class BasicVAE():
 
 class BasicVAE2(BasicVAE):
 
-    ''' KL Divergence from a prior whose parameters can be changed '''
+    ''' KL Divergence from a normal prior whose parameters can be changed '''
     def KLdivergence(self, z_parameters):
         z_mu, z_sigma = z_parameters
         p_mu, p_sigma = 0.0, 1.0
